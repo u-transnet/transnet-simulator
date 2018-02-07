fa1c9b30fabe9f9881370237a7caae89e7e3a30f
Artem <artego@fuit.spbstu.ru>
2018-02-07 21:54:09 +0300

    #23 Add business logic for Logist and Client: buy route map, start trip

--- simulator/src/main/java/com/github/utransnet/simulator/actors/task/ActorTaskContext.java
+++ simulator/src/main/java/com/github/utransnet/simulator/actors/task/ActorTaskContext.java
@@ -1,10 +1,14 @@
 package com.github.utransnet.simulator.actors.task;
 
-import com.github.utransnet.simulator.externalapi.BaseOperation;
-import com.github.utransnet.simulator.externalapi.OperationType;
+import com.github.utransnet.simulator.externalapi.operations.BaseOperation;
+import com.github.utransnet.simulator.externalapi.operations.OperationType;
 import lombok.Getter;
+import lombok.Setter;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
@@ -12,23 +16,32 @@ import java.util.function.Function;
  */
 public class ActorTaskContext {
     @Getter
+    @Nullable
     private final OperationType operationType;
 
     @Getter
     @Nullable
-    private final Function<BaseOperation, Boolean> successPredicate;
+    private final BiFunction<ActorTaskContext, BaseOperation, Boolean> successPredicate;
 
     @Getter
     @Nullable
-    private final Function<BaseOperation, Boolean> failPredicate;
+    private final BiFunction<ActorTaskContext, BaseOperation, Boolean> failPredicate;
 
     @Getter
     private final int waitSeconds;
 
-    public ActorTaskContext(
-            OperationType operationType,
-            @Nullable Function<BaseOperation, Boolean> successPredicate,
-            @Nullable Function<BaseOperation, Boolean> failPredicate,
+    Map<String, Object> payload;
+
+    @Getter
+    @Setter
+    @Nullable
+    Exception exception;
+
+
+    private ActorTaskContext(
+            @Nullable OperationType operationType,
+            @Nullable BiFunction<ActorTaskContext, BaseOperation, Boolean> successPredicate,
+            @Nullable BiFunction<ActorTaskContext, BaseOperation, Boolean> failPredicate,
             int waitSeconds
     ) {
         if(successPredicate == null && waitSeconds == 0) {
@@ -38,24 +51,44 @@ public class ActorTaskContext {
         this.successPredicate = successPredicate;
         this.failPredicate = failPredicate;
         this.waitSeconds = waitSeconds;
+        this.payload = new HashMap<>(10);
     }
 
     public ActorTaskContext(
             OperationType operationType,
-            @Nullable Function<BaseOperation, Boolean> successPredicate,
-            @Nullable Function<BaseOperation, Boolean> failPredicate
+            @Nullable BiFunction<ActorTaskContext, BaseOperation, Boolean> successPredicate,
+            @Nullable BiFunction<ActorTaskContext, BaseOperation, Boolean> failPredicate
     ) {
         this(operationType, successPredicate, failPredicate, 0);
     }
 
     public ActorTaskContext(
             OperationType operationType,
-            @Nullable Function<BaseOperation, Boolean> successPredicate
+            @Nullable BiFunction<ActorTaskContext, BaseOperation, Boolean> successPredicate
     ) {
         this(operationType, successPredicate, null, 0);
     }
 
-    public ActorTaskContext(OperationType operationType, int waitSeconds) {
-        this(operationType, null, null, 0);
+    public ActorTaskContext(int waitSeconds) {
+        this(null, null, null, 0);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getPayload(String key) {
+        Object o = payload.get(key);
+        if(o == null){
+            throw new RuntimeException("Missing value in context");
+        }
+        return (T) o;
+    }
+
+    public ActorTaskContext addPayload(String key, Object value) {
+        payload.put(key, value);
+        return this;
+    }
+
+
+    public static final ActorTaskContext immediate() {
+        return new ActorTaskContext(0);
     }
 }
